8eeeb1cfd2ed3056c8cb3718a89ae8444fdba23d
Sergei Telnov <sergeitelnow@gmail.com>
2020-05-03 01:23:15 +0300

    [code] add probability visualizer

--- python/model_evaluate.py
+++ python/model_evaluate.py
@@ -8,24 +8,21 @@ from python.dataset.data_reader import read_dataset
 from python.dlf.losses import cross_entropy
 
 
-checkpoint_path = '../output/checkpoint/dlf_2997_all__0.25_0.75_0.0001_20200502_1648/cp-{epoch:02d}.ckpt'
+checkpoint_path = '../output/checkpoint/aws/dlf_2997_all__0.25_0.75_0.0001_20200502_1437/cp-{epoch:02d}.ckpt'
 checkpoint_dir = os.path.dirname(checkpoint_path)
 latest = tf.train.latest_checkpoint(checkpoint_dir)
 
 model = DLF()
 model.build(input_shape=([BATCH_SIZE, 16], [BATCH_SIZE, 2]))
 
-test_dataset = read_dataset('../data', 'toy_datasets', is_train=False)
+dataset = read_dataset('../data', 'toy_datasets')
 
-features, bids, target, _ = test_dataset.next()
-survival_rate, _ = model.predict_on_batch([features, bids])
-print(cross_entropy(target, survival_rate))
+idx = 1
+features, bids, target = dataset.next_loss()
+# bids[idx][1] = 5
 
-tvars = model.trainable_variables[:]
 model.load_weights(latest)
 
-survival_rate, _ = model.predict_on_batch([features, bids])
-print(cross_entropy(target, survival_rate))
-
-
-print(tvars == model.trainable_variables)
+h = model.predict_on_batch([features, bids])
+h = h.numpy()[idx]
+print(h)
