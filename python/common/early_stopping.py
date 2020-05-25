ace55bde8543bd66fb4a0571df39bd252e47ca60
Sergei Telnov <sergeitelnow@gmail.com>
2020-05-25 08:42:36 +0300

    [code] fixed early_stopping

--- python/common/early_stopping.py
+++ python/common/early_stopping.py
@@ -13,6 +13,7 @@ class EarlyStopping:
         self.model_name = model_name
         self.min_delta = abs(min_delta)
         self.best = np.Inf
+        self.previous = np.Inf
         self.comp = comp
 
         checkpoint_path = '../output/checkpoint/' + model_name
@@ -27,14 +28,22 @@ class EarlyStopping:
             print('Restore model %s' % model2load)
 
     def check(self, step, loss_score):
-        if abs(self.best - loss_score) < self.min_delta\
-                or not self.comp(self.best * 1.5, loss_score):
+        if abs(self.previous - loss_score) < self.min_delta:
+            print("Training is over")
+            return True
+        self.previous = loss_score
+
+        if not loss_score / self.best > 1.5:
+            print('Overfitting')
+            self.ckpt.restore(self.ckpt_manager.latest_checkpoint)
+            print('Restore best result')
             return True
 
         if self.comp(self.best, loss_score):
             self.best = loss_score
             self._save(step, loss_score)
+        return False
 
     def _save(self, step, score):
         self.ckpt_manager.save()
-        print('Saving checkpoint for step %s with ANLP %s' % (step, score))
+        print('Saving checkpoint in step %s with ANLP %s' % (step, score))
